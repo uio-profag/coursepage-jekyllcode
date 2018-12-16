4d48a6ada2b6e7cea2202ad55630b0a67d79bcf1
Henrik Andersen Sveinsson <henrik.sveinsson@me.com>
2018-12-16 17:58:24 +0100

    Fixed directory traversal issue

--- convert_notebooks.py
+++ convert_notebooks.py
@@ -2,6 +2,7 @@ import os
 import subprocess
 
 path = "pages"
+currentPath = os.path.dirname(os.path.abspath(__file__))
 
 for dirName, subdirList, fileList in os.walk(path):
     print('Found directory: %s' % dirName)
@@ -9,3 +10,4 @@ for dirName, subdirList, fileList in os.walk(path):
         if ".ipynb" in fname: 
             os.chdir(os.path.join(os.path.dirname(__file__), dirName))
             subprocess.call(["jupyter", "nbconvert", fname, "--to", "markdown"])
+            os.chdir(currentPath)
