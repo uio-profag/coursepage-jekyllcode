632c2a59f83007008db5f60843634e226e13c765
Henrik Andersen Sveinsson <henrik.sveinsson@me.com>
2019-01-10 14:06:29 +0100

    removing notebooks after conversion for jekyll to work better

--- convert_notebooks.py
+++ convert_notebooks.py
@@ -11,3 +11,11 @@ for dirName, subdirList, fileList in os.walk(path):
             os.chdir(os.path.join(os.path.dirname(__file__), dirName))
             subprocess.call(["jupyter", "nbconvert", fname, "--to", "markdown"])
             os.chdir(currentPath)
+
+for dirName, subdirList, fileList in os.walk(path):
+    print('Found directory: %s' % dirName)
+    for fname in fileList:
+        if ".ipynb" in fname: 
+            os.chdir(os.path.join(os.path.dirname(__file__), dirName))
+            subprocess.call(["rm", fname])
+            os.chdir(currentPath)
